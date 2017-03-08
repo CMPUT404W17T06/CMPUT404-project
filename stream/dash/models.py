6589fffd3f99a2b30da7c167a3b1e29719f7c9e0
Braedy Kuzma <braedy@ualberta.ca>
2017-03-07 19:08:36 -0700

    Weird database things. I'm sorry in advance.

--- stream/dash/models.py
+++ stream/dash/models.py
@@ -5,9 +5,6 @@ from django.conf import settings
 import django.utils.timezone as timezone
 import uuid
 
-def uuidHex():
-    return uuid.uuid4().hex
-
 """class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -37,7 +34,7 @@ class Post(models.Model):
     published = models.DateTimeField(default=timezone.now)
 
     # This should really have a validator
-    uuid = models.CharField('id', max_length=32, default=uuidHex,
+    uuid = models.CharField('id', max_length=36, default=uuid.uuid4,
                             primary_key=True)
     visibility = models.CharField(max_length=10, default="PUBLIC")
     visibleTo = models.ManyToManyField(settings.AUTH_USER_MODEL,
