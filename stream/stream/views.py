5c6ef1cb2192f8649de81435d6a8cfa7edd99b44
Noah Shillington <nshillin@ualberta.ca>
2017-03-07 18:47:51 -0700

    Added post stream
    Can be accessed through /stream . As far as I can tell, I didn't
    organize it correctly in the directory, I'll fix that tomorrow.

--- stream/stream/views.py
+++ stream/stream/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views import generic
+from dash.models import Post
+
+class StreamView(generic.ListView):
+    template_name = 'stream.html'
+    context_object_name = 'latest_post_list'
+    def get_queryset(self):
+        #Return the last five published questions
+        return Post.objects.order_by('published')[:5]
+
+def stream(request):
+    latest_post_list = Post.objects.order_by('-published')[:5]
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'polls/index.html',context)
+
 
 @login_required(login_url="login/")
 def home(request):
