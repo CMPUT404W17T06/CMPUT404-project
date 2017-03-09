d97a4b3b8b35494dff7024ca590df27e49b187d6
Panayioti Koukoulas <koukoula@cs.ualberta.ca>
2017-03-09 16:21:35 -0700

    filtered to display public and serveronly

--- stream/dash/views.py
+++ stream/dash/views.py
@@ -4,14 +4,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views import generic
 from .models import Post
+from django.db.models import Q
 
 class StreamView(generic.ListView):
     template_name = 'dashboard.html'
     context_object_name = 'latest_post_list'
     def get_queryset(self):
         #Return the last five published questions
-        return Post.objects.order_by('published')[:5]
-
+        querySet = Post.objects.filter(  
+            Q(visibility='PUBLIC') | Q(visibility='SERVERONLY') 
+        )
+        return querySet[:5]
+        #return Post.objects.order_by('published')[:5]
+        #CURRENTLY DOESNT DISPLAY ALL POSTS BY YOU FOR TESTING
 @login_required(login_url="login/")
 def dashboard(request):
     return render(request,"dashboard.html")
