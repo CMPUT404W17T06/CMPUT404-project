1888ec1f1cca98bfccf88b125b26008c16770dfb
aolin1 <aolin1@ualberta.ca>
2017-03-10 14:30:36 -0700

    admin friendrequests

--- stream/dash/admin.py
+++ stream/dash/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment, Author, AuthorFriends, Category, CanSee
-#,FriendRequest
+from .models import Post, Comment, Author, AuthorFriends, Category, CanSee,FriendRequest
 # Register your models here.
 
 admin.site.register(Author)
@@ -9,5 +8,4 @@ admin.site.register(Comment)
 admin.site.register(AuthorFriends)
 admin.site.register(Category)
 admin.site.register(CanSee)
-
-#admin.site.register(FriendRequest)
+admin.site.register(FriendRequest)
