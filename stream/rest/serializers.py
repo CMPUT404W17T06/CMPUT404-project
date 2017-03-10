c0b5720055987215a325b009a1cf7f32181840d0
Braedy Kuzma <braedy@ualberta.ca>
2017-03-09 19:49:58 -0700

    Add serializers for a few objects.

--- stream/rest/serializers.py
+++ stream/rest/serializers.py
@@ -1,10 +1,37 @@
 # Author: Braedy Kuzma
 
 from rest_framework import serializers
-from dash.models import Post
+from dash.models import Post, Author, Comment, Category
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('id', 'host', 'url')
+
+    def to_representation(self, author):
+        rv = serializers.ModelSerializer.to_representation(self, author)
+        rv['displayName'] = author.user.get_username()
+        return rv
+
+class CategorySerializer(serializers.BaseSerializer):
+    def to_representation(self, category):
+        return category.category
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'source', 'origin', 'description', 'contentType',
-                  'content', 'author', 'categories')
+        fields = '__all__'
+    author = AuthorSerializer()
+
+    def to_representation(self, post):
+        rv = serializers.ModelSerializer.to_representation(self, post)
+        categories = Category.objects.filter(post=post)
+        catSer = CategorySerializer(categories, many=True)
+        rv['categories'] = catSer.data
+        return rv
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('author', 'comment', 'contentType', 'published', 'id')
+    author = AuthorSerializer()
