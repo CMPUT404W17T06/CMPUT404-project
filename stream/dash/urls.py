4231950b698bab194f75b1e04ea00a2bea369c74
aolin1 <aolin1@ualberta.ca>
2017-03-10 20:52:03 -0700

    Update urls.py

--- stream/dash/urls.py
+++ stream/dash/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.StreamView.as_view(), name='dash'),
     url(r'^newpost/$', views.newPost, name='newpost'),
     url(r'^newcomment/$', views.newComment, name='newcomment'),
+    url(r'^friends/?$', views.friend_handler, name='friend_handler'),
+    url(r'^friendrequest/?$', views.friendrequest_handler, name='friendrequest_handler'),
 ]
