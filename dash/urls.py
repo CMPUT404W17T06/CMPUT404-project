e3c8e847f4cc5a0c36f348135e7d5843544c47e2
aolin1 <aolin1@ualberta.ca>
2017-03-12 15:11:35 -0600

    Add files via upload

--- dash/urls.py
+++ dash/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^$', views.StreamView.as_view(), name='dash'),
     url(r'^newpost/$', views.newPost, name='newpost'),
     url(r'^newcomment/$', views.newComment, name='newcomment'),
+    url(r'^friends/(?P<author1_id>[0-9a-z-]+)/(?P<author2_id>[0-9a-z-]+)/?$', views.friend_query_handler, name='friend_query_handler'),
+    url(r'^friends/(?P<id>[^/]+)/?$', views.friend_handler_specific, name='friend_handler_specific'),
+    url(r'^friends/?$', views.friend_handler, name='friend_handler'),
+    url(r'^friendrequest/?$', views.friendrequest_handler, name='friendrequest_handler'),
 ]
