1577b5046d4ba40302308c202b379b2356661157
Ape-With-a-keyboard <vinsonn2002@gmail.com>
2017-03-11 17:22:06 -0700

    fixing log in

--- log/views.py
+++ log/views.py
@@ -56,35 +56,4 @@ def update_profile(request):
 	return render(request, 'profile.html', {
 		'profile_form': profile_form
 	})
-@login_required(login_url="login/")
-def view_profile(request, id):
-	if (request.method == 'GET'):
-		host = request.GET.get('host', '')
-		if (host != 'https://cmput404t06.herokuapp.com/dash/'):
-			nodes = Nodes.objects.all()
-			json_profile = {}
-			for node in nodes:
-				if (node.url == host):
-					if (node.useauth):
-						response = requests.get(node.url + "author/" + str(id), auth=HTTPBasicAuth(node.username, node.password))
-					else:
-						response = requests.get(node.url + "author/", auth=HTTPBasicAuth(node.username, node.password))
-						
-					json_profile = json.loads(response.content)
-					break
-			user = request.user
-			request_id = user.author.id
-			if not json_profile:
-				return HttpResponse(status=404)
-			else:
-				json_profile['url'] = json_profile['host'] + 'author/' + str(json_profile['id'])
-				return render(request, 'author.html', {'author': json_profile, 'user_id': request_id, 'request_user': user, 'profile_user': json_profile})
-		else:
-			author = Author.objects.get(id=id)
-			display = Author.objects.get(id=id)
-			user = request.user
-			request_id = user.author.id
-			author.url = author.host + 'author/' + str(author.id)
-			return render(request, 'author.html', {'author':author, 'user_id':request_id,'request_user':user, 'profile_user':display })
 
-	return HttpResponse(status=405)
