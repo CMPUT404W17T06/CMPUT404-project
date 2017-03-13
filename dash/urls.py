015233dd82fc58ee8745061379c899c43c6525ba
Noah Shillington <nshillin@ualberta.ca>
2017-03-12 19:04:00 -0600

    removed redundancy

--- dash/urls.py
+++ dash/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', login_required(views.StreamView.as_view()), name='dash'),
+    url(r'^$', views.StreamView.as_view(), name='dash'),
     url(r'^newpost/$', views.newPost, name='newpost'),
     url(r'^newcomment/$', views.newComment, name='newcomment'),
     url(r'^friends/(?P<author1_id>[0-9a-z-]+)/(?P<author2_id>[0-9a-z-]+)/?$', views.friend_query_handler, name='friend_query_handler'),
