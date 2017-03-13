b7145e7a0dd0bb9dfd8d29feef7106c076dcf439
Noah Shillington <nshillin@ualberta.ca>
2017-03-13 00:56:16 -0600

    gave github activity scroll view, allowed for more results to be loaded

--- stream/static/js/github.js
+++ stream/static/js/github.js
@@ -22,19 +22,43 @@ function sendJSONXMLHTTPRequest(url, callback) {
 
 function handleGithubJSON(parsedText) {
   var template = document.getElementById("git_template");
-  var stream = document.getElementById('git_stream');
-  for (var i = parsedText.length-1; i >= 0; i--) {
+  var stream = document.getElementById('git_events');
+  if (parsedText.length < 30) {
+    $('#git_button').hide();
+  }
+  for (var i = 0; i < parsedText.length; i++) {
     var newGit = template.content.cloneNode(true).childNodes[1];
     newGit.getElementsByClassName('title')[0].innerHTML = parsedText[i].repo.name + ': ' + parsedText[i].type
     newGit.getElementsByClassName('creator')[0].innerHTML = parsedText[i].actor.display_login
     newGit.getElementsByClassName('creation_date')[0].innerHTML = parsedText[i].created_at
-    stream.insertBefore(newGit,stream.childNodes[0]);
+    stream.append(newGit,stream.childNodes[0]);
   }
 }
 
 var username = '';
+var page = 1;
 
+function loadPage(url) {
+  username = url.replace('https://github.com/','');
+  var url = 'https://api.github.com/users/' + username + '/events/public?page=';
+  var url_page = url + String(page)
+  sendJSONXMLHTTPRequest(url_page,handleGithubJSON)
+  page++;
+}
+
+/*
 function parseGithubURL(url) {
   username = url.replace('https://github.com/','');
   return 'https://api.github.com/users/' + username + '/events/public';
 }
+*/
+/*
+function goThroughPages(url) {
+  username = url.replace('https://github.com/','');
+  var url = 'https://api.github.com/users/' + username + '/events/public?page=';
+  for (var i=1; i<=10; i++) {
+    var url_page = url + String(i)
+    sendJSONXMLHTTPRequest(url_page,handleGithubJSON)
+  }
+}
+*/
