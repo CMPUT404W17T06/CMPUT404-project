7c2e5c953ec3244d416a0379fc742ff959a7d14d
Noah Shillington <nshillin@ualberta.ca>
2017-03-13 10:21:40 -0600

    Added make post test

--- dash/tests.py
+++ dash/tests.py
@@ -3,17 +3,20 @@ from django.test.utils import setup_test_environment
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db.utils import IntegrityError
-from dash.models import Author
+from dash.models import Author, Post, Comment
+from dash.forms import PostForm, CommentForm
+from django.forms.models import model_to_dict
 import requests
 import uuid
 
 # Create your tests here.
-u = 'test'
-p = '12345678'
 
 # https://docs.djangoproject.com/en/1.10/intro/tutorial05/
+# https://docs.djangoproject.com/en/1.10/topics/testing/tools/
 class DashViewTests(TestCase):
     def setUp(self):
+        u = 'test'
+        p = '12345678'
         user = User()
         user.username = u
         user.set_password(p)
@@ -24,8 +27,9 @@ class DashViewTests(TestCase):
         user.save()
         author = Author()
         author.user = user
+        author.github = 'https://github.com/nshillin'
         author.host = 'http://127.0.0.1/'
-        author.id = 'http://127.0.0.1/author/' + uuid.uuid4().hex
+        author.id = author.host + 'author/' + uuid.uuid4().hex
         author.url = author.id
         author.username = u
         author.save()
@@ -36,3 +40,29 @@ class DashViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No posts available.")
         self.assertQuerysetEqual(response.context['latest_post_list'], [])
+
+    def test_make_post(self):
+        data = {
+            'title': 'Test',
+            'description': 'Test',
+            'contentType': 'Test',
+            'content': 'Test',
+            'visibility': 'PUBLIC'
+        }
+        post_response = self.client.post("/dash/newpost/", data)
+        self.assertEqual(post_response.status_code, 302)
+
+        response = self.client.get('/dash/')
+        self.assertEqual(response.status_code, 200)
+
+        post = model_to_dict(response.context['latest_post_list'][0])
+        for i in data:
+            self.assertEqual(post[i],data[i])
+
+    def test_comment_on_post(self):
+        pass
+
+    def test_github_activity(self):
+        """ Not certain of the best way to test this yet """
+        #response = self.client.get('/dash/')
+        #self.assertContains(response, "PushEvent")
