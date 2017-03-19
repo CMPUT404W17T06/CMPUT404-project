011e255c49ed6b2c528e33d64faafbf4d1b6a8a0
Braedy Kuzma <braedy@ualberta.ca>
2017-03-18 23:47:21 -0600

    Better visibleTo validation.
    Also new interface for validators.

--- rest/utils.py
+++ rest/utils.py
@@ -48,6 +48,13 @@ class InvalidField(DefaultException):
     def __init__(self, name, value):
         DefaultException.__init__(self, {name: value}, 400)
 
+class DependencyError(DefaultException):
+    """
+    Exception for a field with a dependency on another failing.
+    """
+    def __init__(self, values):
+        DefaultException.__init__(self, values, 400)
+
 class NotFound(DefaultException):
     """
     Exception for a request that was looking for a resource that couldn't be
@@ -93,7 +100,7 @@ def exceptionHandler(exc, context):
 
 # Only compile this once because it's always the same and they're intensive
 __imageContentTypeRE = re.compile(r'image/\w*\s*;\s*base64')
-def validateContentType(name, contentType):
+def validateContentType(data, name, contentType):
     """
     Clean up contentType and ensure it's one of text/plain, text/markdown,
     image/*;base64.
@@ -113,7 +120,7 @@ def validateContentType(name, contentType):
 
     return contentType
 
-def validateAuthorExists(name, authorId):
+def validateAuthorExists(data, name, authorId):
     """
     Verify that the author id exists in our database.
     """
@@ -122,7 +129,7 @@ def validateAuthorExists(name, authorId):
         raise InvalidField(name, authorId)
     return authorId
 
-def validateVisibility(name, visibility):
+def validateVisibility(data, name, visibility):
     """
     Verify that visibility is one of PUBLIC, FOAF, FRIENDS, PRIVATE, SERVERONLY.
     """
@@ -130,7 +137,7 @@ def validateVisibility(name, visibility):
         raise InvalidField(name, visibility)
     return visibility
 
-def validateDate(name, published):
+def validateDate(data, name, published):
     """
     Verify that a date string is valid.
     """
@@ -140,7 +147,7 @@ def validateDate(name, published):
         raise InvalidField(name, published)
     return published
 
-def validateBool(name, value):
+def validateBool(data, name, value):
     """
     Verify that a string could be translated to a valid bool.
     """
@@ -149,7 +156,7 @@ def validateBool(name, value):
         raise InvalidField(name, value)
     return value
 
-def validateList(name, value):
+def validateList(data, name, value):
     """
     Verify that the value is a list.
     """
@@ -157,7 +164,7 @@ def validateList(name, value):
         raise InvalidField(name, value)
     return value
 
-def validateURL(name, value):
+def validateURL(data, name, value):
     """
     Verify that a string is a URL.
     """
@@ -169,18 +176,26 @@ def validateURL(name, value):
         raise InvalidField(name, value)
     return value
 
-def validateURLList(name, value):
+def validateURLList(data, name, value):
     """
     Validate that a value is a list and that it contains URLs.
     """
     validateList(name, value)
     for i, url in enumerate(value):
         try:
-            value[i] = validateURL('', url)
+            value[i] = validateURL({}, '', url)
         # Catch the InvalideField for the URL and raise our own for the list
         except InvalidField:
             raise InvalidField(name, value)
     return value
+
+def validateVisibleTo(data, name, value):
+    visibility = data['visibility']
+    if visibility != 'PRIVATE':
+        raise DependencyError({'visibility': visibility, name: value})
+
+    # No need to catch and override any errors from this
+    validateURLList(data, name, value)
 
 # Fields we can validate on incoming data for posts
 postValidators = (
@@ -193,5 +208,5 @@ postValidators = (
     ('visibility', validateVisibility),
     ('unlisted', validateBool),
     ('categories', validateList),
-    ('visibleTo', validateURLList)
+    ('visibleTo', validateVisibleTo)
 )
