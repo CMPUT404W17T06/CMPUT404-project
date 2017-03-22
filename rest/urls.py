fda79e91b6c8f29d1456028a9a61a2e25d34f2e7
Braedy Kuzma <braedy@ualberta.ca>
2017-03-22 14:52:24 -0600

    Add url for multiple posts endpoint.

--- rest/urls.py
+++ rest/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^posts/(?P<pid>[0-9a-fA-F\-]+)/$', views.PostView.as_view(), name='post'),
+    url(r'^posts/$', views.PostsView.as_view(), name='posts')
 ]
