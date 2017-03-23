bcbdf0281c8310785b4793b8ab6e48a65b122d1e
Braedy Kuzma <braedy@ualberta.ca>
2017-03-23 14:47:08 -0600

    Add author endpoint.

--- rest/urls.py
+++ rest/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^posts/(?P<pid>[0-9a-fA-F\-]+)/$', views.PostView.as_view(), name='post'),
-    url(r'^posts/$', views.PostsView.as_view(), name='posts')
+    url(r'^posts/(?P<pid>[0-9a-fA-F\-]+)/$', views.PostView.as_view(),
+        name='post'),
+    url(r'^posts/$', views.PostsView.as_view(), name='posts'),
+    url(r'^author/(?P<aid>[0-9a-fA-F\-]+)/$', views.AuthorView.as_view(),
+        name='author')
 ]
