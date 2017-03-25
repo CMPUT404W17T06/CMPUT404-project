0dd2bd0a8d2b041672afdf66666df63e2dd1a044
Braedy Kuzma <braedy@ualberta.ca>
2017-03-25 15:29:39 -0600

    Add author friends url.
    Also reordered to make sense.

--- rest/urls.py
+++ rest/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^posts/$', views.PostsView.as_view(), name='posts'),
     url(r'^posts/(?P<pid>[0-9a-fA-F\-]+)/$', views.PostView.as_view(),
         name='post'),
-    url(r'^posts/$', views.PostsView.as_view(), name='posts'),
+    url(r'^posts/(?P<pid>[0-9a-fA-F\-]+)/comments/$',
+        views.CommentView.as_view(), name='comments'),
     url(r'^author/(?P<aid>[0-9a-fA-F\-]+)/$', views.AuthorView.as_view(),
         name='author'),
-    url(r'^posts/(?P<pid>[0-9a-fA-F\-]+)/comments/$',
-        views.CommentView.as_view(), name='comments')
+    url(r'^author/(?P<aid>[0-9a-fA-F\-]+)/friends/$',
+        views.AuthorFriendsView.as_view(), name='friends')
 ]
