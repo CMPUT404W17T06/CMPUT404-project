f8c26bb11a3d064902fd6bb70b9933861ce7fe1d
Braedy Kuzma <braedy@ualberta.ca>
2017-03-25 14:05:06 -0600

    Add one place to get JSONResponse from.

---
+++ rest/httpUtils.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+
+# Initially taken from
+# http://www.django-rest-framework.org/tutorial/1-serialization/
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json; charset=utf-8'
+        super(JSONResponse, self).__init__(content, **kwargs)
