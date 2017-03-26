9c73b646b4a5fb4d82177cc4047eedddecdcbc00
Braedy Kuzma <braedy@ualberta.ca>
2017-03-25 21:30:06 -0600

    Add remote comment author to admin panel.

--- dash/admin.py
+++ dash/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Post, Comment, Author, Category, CanSee, FriendRequest, \
-                    Follow
+                    Follow, RemoteCommentAuthor
 
 # Register your models here.
 
 admin.site.register(Author)
+admin.site.register(RemoteCommentAuthor)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
